178352be61dd272109a21df9dfe0e0524fc1e22d
Jessi Chavez Gallego <angelwerkengineeringmail.com>
2023-02-16 20:51:22 -0800

    added web docs

--- src/js/record.js
+++ src/js/record.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 import { readFile } from 'node:fs/promises';
 
+/**
+ * This function adds one to its input.
+ * @param {number} input any number
+ * @returns {number} that number, plus one.
+ */
+
 async function getFileRecords(file) {
   if (fs.existsSync(file)) {
     try {
